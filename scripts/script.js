b01a6baf4e3d9818777e2283281a17f2a085be52
lilycm <lilychakma733@gmail.com>
2024-02-18 10:32:33 +0600

    added a function

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,15 @@
 
 const allSeatNumber = document.getElementsByClassName('seat-number');
 
-for(const seat of allSeatNumber){
-    seat.addEventListener('click', function(e){
+let seatCount = 0;
 
-        const seatNumber = e.target.parentNode.childNodes[1].innerText
-        console.log();
+for (const seat of allSeatNumber) {
+    seat.addEventListener('click', function (e) {
+        seatCount = seatCount + 1;
+        document.getElementById('seat-count').innerText = seatCount;
+
+        e.target.disabled = true;
+        const seatNumber = e.target.parentNode.childNodes[1].innerText;
 
         const seatDetail = document.getElementById('seat-details');
 
@@ -16,15 +20,26 @@ for(const seat of allSeatNumber){
         const p = document.createElement('p');
         p.innerText = seatNumber;
 
-        const p2= document.createElement('p');
-        p2.innerText= 'Economoy';
+        const p2 = document.createElement('p');
+        p2.innerText = 'Economoy';
 
-        const p3= document.createElement('p');
-        p3.innerText= '550';
+        const p3 = document.createElement('p');
+        p3.innerText = '550';
 
-        li.appendChild(p)
+        li.appendChild(p);
+        li.appendChild(p2);
+        li.appendChild(p3);
         seatDetail.appendChild(li);
-        // seatDetail.appendChild(p2);
-        // seatDetail.appendChild(p3);
+        
     })
+}
+
+function setBackgroundColor(elementId) {
+    const element = document.getElementsByClassName(elementId);
+    element.classlist.add('bg-green-500');
+}
+
+function removeBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-[#F7F8F8]')
 }
