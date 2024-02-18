e4942a4a0f9edb5c47bac7afe57325d483df3fcf
lilycm <lilychakma733@gmail.com>
2024-02-18 11:22:52 +0600

    added total price function

--- scripts/script.js
+++ scripts/script.js
@@ -13,27 +13,37 @@ for (const seat of allSeatNumber) {
 
         seatCount = seatCount + 1;
         document.getElementById('seat-count').innerText = seatCount;
-
         e.target.disabled = true;
-        const seatNumber = e.target.parentNode.childNodes[1].innerText;
 
+        const seatNumber = e.target.parentNode.childNodes[1].innerText;
         const seatDetail = document.getElementById('seat-details');
 
         const li = document.createElement('li');
 
         const p = document.createElement('p');
         p.innerText = seatNumber;
-
         const p2 = document.createElement('p');
         p2.innerText = 'Economoy';
 
         const p3 = document.createElement('p');
-        p3.innerText = '550';
+        p3.innerText = 550;
 
         li.appendChild(p);
         li.appendChild(p2);
         li.appendChild(p3);
         seatDetail.appendChild(li);
+
+        const fixedPrice = document.getElementById('fixed-price').innerText;
+        const convertedFixedPrice = parseInt(fixedPrice);
+        document.getElementById('fixed-price').innerText = convertedFixedPrice;
+
+        const totalPrice = document.getElementById('total-price').innerText;
+        const convertedTotalPrice = parseInt(totalPrice);
+        const sum = convertedTotalPrice + convertedFixedPrice;
+        document.getElementById('total-price').innerText = sum;
+        console.log(sum);
+
+
 
         const totalSeat = document.getElementById('total-seat').innerText;
         const convertedTotalSeat = parseInt(totalSeat);
